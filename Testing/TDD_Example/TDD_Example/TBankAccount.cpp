9a35bd6a20c070f854790442c7885dea83eb7e96
rsr <rsr@ableton.com>
2025-09-05 11:59:05 +0200

    TDD: Add test for making a first deposit (doesn't compile)

--- Testing/TDD_Example/TDD_Example/TBankAccount.cpp
+++ Testing/TDD_Example/TDD_Example/TBankAccount.cpp
@@ -11,10 +11,14 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 
+static const double delta = 0.0000001;
+
+
 class UnnamedTestSuite : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(UnnamedTestSuite);
 //    CPPUNIT_TEST(testExample);
     CPPUNIT_TEST(testCheckInitialBalanceIs0);
+    CPPUNIT_TEST(testMakeDepositAddsAmountToBalance);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -29,6 +33,15 @@ public:
     void testCheckInitialBalanceIs0() {
         BankAccount account;
         CPPUNIT_ASSERT_EQUAL(account.getBalance(), 0.);
+    }
+    
+    void testMakeDepositAddsAmountToBalance() {
+        BankAccount account;
+        
+        static constexpr double amount = 512.36;
+        account.deposit(amount);
+        
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(account.getBalance(), amount, delta);
     }
 
 };

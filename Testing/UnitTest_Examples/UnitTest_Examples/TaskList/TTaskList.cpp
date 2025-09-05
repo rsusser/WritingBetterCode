0f34ed2b14244dee510b1f3f6361ce92ff9ddc60
rsr <rsr@ableton.com>
2025-09-05 15:35:01 +0200

    UT: Properly isolate tests so state changes don't influence each other

--- Testing/UnitTest_Examples/UnitTest_Examples/TaskList/TTaskList.cpp
+++ Testing/UnitTest_Examples/UnitTest_Examples/TaskList/TTaskList.cpp
@@ -11,10 +11,9 @@
 
 namespace TTaskList {
 
-TaskList tasks;
-
 
 TEST(TaskList, AddTask) {
+    TaskList tasks;
     tasks.add({"Do laundry", false});
     
     EXPECT_EQ(tasks.count(), 1lu); // does tasks.count() == 1 ?
@@ -23,24 +22,30 @@ TEST(TaskList, AddTask) {
 
 
 TEST(TaskList, AddEmptyTask) {
+    TaskList tasks;
     tasks.add({"", false});
     
-    EXPECT_EQ(tasks.count(), 2lu);
-    EXPECT_EQ(tasks.getAtIndex(1).title, std::string(""));
+    EXPECT_EQ(tasks.count(), 1lu);
+    EXPECT_EQ(tasks.getAtIndex(0).title, std::string(""));
 }
 
 
 TEST(TaskList, AddMultipleTasks) {
+    TaskList tasks;
     tasks.add({"Clean shower", false});
     tasks.add({"Study physics", true});
     
-    EXPECT_EQ(tasks.count(), 3lu);
-    EXPECT_EQ(tasks.getAtIndex(1).title, std::string("Clean shower"));
-    EXPECT_EQ(tasks.getAtIndex(2).title, std::string("Study physics"));
+    EXPECT_EQ(tasks.count(), 2lu);
+    EXPECT_EQ(tasks.getAtIndex(0).title, std::string("Clean shower"));
+    EXPECT_EQ(tasks.getAtIndex(1).title, std::string("Study physics"));
 }
 
 
 TEST(TaskList, CheckDoneTasks) {
+    TaskList tasks;
+    tasks.add({"Clean shower", false});
+    tasks.add({"Study physics", true});
+
     int doneCount = 0;
     for (auto i = 0; i < tasks.count(); ++i) {
         if (tasks.getAtIndex(i).done) ++doneCount;

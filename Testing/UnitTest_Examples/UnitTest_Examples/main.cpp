b61207277538e8569295938979ac4f8618bf4a5e
rsr <rsr@ableton.com>
2025-09-05 15:00:12 +0200

    Add googletest submodule

---
+++ Testing/UnitTest_Examples/UnitTest_Examples/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  UnitTest_Examples
+//
+//  Created by Rachel Susser on 05.09.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
